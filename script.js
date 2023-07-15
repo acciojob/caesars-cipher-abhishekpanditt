b06657a7cee29334a617b5dd5dcad1018d983dd0
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-15 16:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,27 +31,26 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  var decodedString = "";
 
-  for (var i = 0; i < encodedString.length; i++) {
-    var charCode = encodedString.charCodeAt(i);
+let A = "A".charCodeAt(0); /// 65
+let Z = "Z".charCodeAt(0); // 90
 
-    if (charCode >= 65 && charCode <= 90) {
-      // If it's an uppercase letter (A-Z)
-      var decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
-      decodedString += String.fromCharCode(decodedCharCode);
-    } else {
-      // Non-alphabetic characters
-      decodedString += encodedString.charAt(i);
-    }
-  }
-
-  return decodedString;
+function rot13(str) {
+	let ans = "" ; // "" => "$N#A"
+	// str = "$A#N" => "$N#A"
+	for(let i = 0; i < str.length; i++){
+		// i = 0
+		let currentChar = str[i].charCodeAt(0);
+		if(currentChar <= Z && currentChar >= A){
+			ans += lookup[str[i]]
+		}
+		else {
+			ans += str[i] ;
+		}
+	}
+	return ans ;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-console.log(rot13("SERR YBIR? NPPVBWBO"));
+  
 
 // Do not change this line
 window.rot13 = rot13;
